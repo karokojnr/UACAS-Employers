90c80714903eff91964324e880ccd64b4e9bddae
karokojnr <karoko.jnr99@gmail.com>
2020-11-19 10:58:28 +0300

    Embellishment :tropical_drink:

--- config/auth.js
+++ config/auth.js
@@ -5,7 +5,7 @@ module.exports = {
     if (req.isAuthenticated()) {
       return next();
     }
-    req.flash("error_msg", "Please log in to view that resource");
+    req.flash("error_msg", "Please log in to continue...");
     res.redirect("/users/login");
   },
   forwardAuthenticated: function(req, res, next) {
